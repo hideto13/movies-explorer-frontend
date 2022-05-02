b35f11718a4da6644bb289749a490cfa3c1a1090
Tanya <pinki_00@mail.ru>
2022-05-02 11:37:42 +0300

    Header styles

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,7 +1,48 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Navigation.css";
 
 function Navigation() {
-  return <div>Navigation</div>;
+  const [navIsOpen, setNavIsOpen] = useState(false);
+
+  const navigationClassName = `navigation ${
+    navIsOpen ? "navigation_visible" : ""
+  }`;
+  return (
+    <>
+      <nav className={navigationClassName}>
+        <span
+          className="navigation__close"
+          onClick={() => {
+            setNavIsOpen(false);
+          }}
+        ></span>
+        <div className="navigation__list">
+          <Link to="/" className="navigation__item navigation__item_main">
+            Главная
+          </Link>
+          <Link
+            to="/movies"
+            className="navigation__item navigation__item_active"
+          >
+            Фильмы
+          </Link>
+          <Link to="/saved-movies" className="navigation__item">
+            Сохраненные фильмы
+          </Link>
+        </div>
+        <Link className="navigation__account" to="/profile">
+          Аккаунт
+        </Link>
+      </nav>
+      <button
+        className="navigation__menu"
+        onClick={() => {
+          setNavIsOpen(true);
+        }}
+      ></button>
+    </>
+  );
 }
 
 export default Navigation;
