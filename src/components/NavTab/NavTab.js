042ff9a74c7130b4a676abd45098acea52c3b6d9
Tanya <pinki_00@mail.ru>
2022-05-02 16:01:18 +0300

    Add promo section

--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -1,7 +1,25 @@
 import "./NavTab.css";
 
 function NavTab() {
-  return <div>NavTab</div>;
+  return (
+    <ul className="navtab">
+      <li className="navtab__item">
+        <a className="navtab__link" href="#">
+          О проекте
+        </a>
+      </li>
+      <li className="navtab__item">
+        <a className="navtab__link" href="#">
+          Технологии
+        </a>
+      </li>
+      <li className="navtab__item">
+        <a className="navtab__link" href="#">
+          Студент
+        </a>
+      </li>
+    </ul>
+  );
 }
 
 export default NavTab;
