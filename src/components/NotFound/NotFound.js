88f03c77c5bf376bfec51e471e4f4464a8089471
Tanya <pinki_00@mail.ru>
2022-05-02 13:15:25 +0300

    Not found page

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom";
 import "./NotFound.css";
 
 function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <section className="not-found">
+      <h1 className="not-found__title">404</h1>
+      <p className="not-found__text">Страница не найдена</p>
+      <Link to="/" className="not-found__link">
+        Назад
+      </Link>
+    </section>
+  );
 }
 
 export default NotFound;
