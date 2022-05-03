ebfa1f4289f0f76f7ef623a7e0b50fb93f2de995
Tanya <pinki_00@mail.ru>
2022-05-03 19:14:57 +0300

    Add about me section

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,7 +1,42 @@
+import Portfolio from "../Portfolio/Portfolio";
+import photo from "../../images/photo.jpg";
 import "./AboutMe.css";
 
 function AboutMe() {
-  return <div>AboutMe</div>;
+  return (
+    <section className="about-me">
+      <h2 className="about-me__title">Студент</h2>
+      <div className="about-me__container">
+        <div className="about-me__info">
+          <h3 className="about-me__name">Таня</h3>
+          <p className="about-me__description">Фронтенд-разработчик, 28 лет</p>
+          <p className="about-me__about">
+            Я родилась и живу в Клину, закончила физико-математический факультет
+            РУДН. У меня есть дочь. Я люблю изучать новое, читать книги и
+            слушать музыку. Начала кодить в 2018 году. По первому образованию я
+            медсестра, раньше я работала в стоматологии. В 2020 году получила
+            работу в IT компании в качестве сотрудника техподдержки. После того
+            как начала проходить курс по веб-разработке, получила повышение до
+            фронтенд-разработчика.
+          </p>
+          <ul className="about-me__socials">
+            <li className="socials__item">
+              <a className="socials__link" href="#">
+                Facebook
+              </a>
+            </li>
+            <li className="socials__item">
+              <a className="socials__link" href="#">
+                Github
+              </a>
+            </li>
+          </ul>
+        </div>
+        <img className="about-me__photo" src={photo} alt="photo"></img>
+      </div>
+      <Portfolio />
+    </section>
+  );
 }
 
 export default AboutMe;
