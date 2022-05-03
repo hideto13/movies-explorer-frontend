12a7a3f31c961cabdf35da480f32aed7818494fd
Tanya <pinki_00@mail.ru>
2022-05-03 20:00:45 +0300

    Add footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,33 @@
 import "./Footer.css";
 
 function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="footer">
+      <h3 className="footer__title">
+        Учебный проект Яндекс.Практикум х BeatFilm.
+      </h3>
+      <div className="footer__main">
+        <p className="footer__copyright">&copy; 2022</p>
+        <ul className="footer__socials">
+          <li className="footer__social">
+            <a className="footer__link" href="#">
+              Яндекс.Практикум
+            </a>
+          </li>
+          <li className="footer__social">
+            <a className="footer__link" href="#">
+              Github
+            </a>
+          </li>
+          <li className="footer__social">
+            <a className="footer__link" href="#">
+              Facebook
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
