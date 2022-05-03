0049a71da24222a04145b7c71adcd96ee11eaab5
Tanya <pinki_00@mail.ru>
2022-05-03 23:22:23 +0300

    Add profile page

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,14 +1,31 @@
 import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
 import "./Profile.css";
 
 function Profile() {
   return (
     <>
       <Header />
-
-      <div>Profile</div>
-      <Footer />
+      <section className="profile">
+        <h1 className="profile__title">Привет, Таня!</h1>
+        <ul className="profile__list">
+          <li className="profile__item">
+            <p className="profile__description">Имя</p>
+            <p className="profile__text">Таня</p>
+          </li>
+          <li className="profile__item">
+            <p className="profile__description">E-mail</p>
+            <p className="profile__text">pochta@yandex.ru</p>
+          </li>
+        </ul>
+        <div className="profile__buttons">
+          <button className="profile__button profile__button_edit">
+            Редактировать
+          </button>
+          <button className="profile__button profile__button_exit">
+            Выйти из аккаунта
+          </button>
+        </div>
+      </section>
     </>
   );
 }
