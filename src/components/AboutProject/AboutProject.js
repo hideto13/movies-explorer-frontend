3bc1f63229d0ae9d7418b58cdc2c83d8c2ea16fe
Tanya <pinki_00@mail.ru>
2022-05-03 15:44:39 +0300

    Add about-project section

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -1,7 +1,43 @@
 import "./AboutProject.css";
 
 function AboutProject() {
-  return <div>AboutProject</div>;
+  return (
+    <section className="about-project">
+      <h2 className="about-project__title">О проекте</h2>
+      <ul className="about-project__list">
+        <li className="about-project__item">
+          <h3 className="about-project__subtitle">
+            Дипломный проект включал 5 этапов
+          </h3>
+          <p className="about-project__text">
+            Составление плана, работу над бэкендом, вёрстку, добавление
+            функциональности и финальные доработки.
+          </p>
+        </li>
+        <li className="about-project__item">
+          <h3 className="about-project__subtitle">
+            На выполнение диплома ушло 5 недель
+          </h3>
+          <p className="about-project__text">
+            У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
+            соблюдать, чтобы успешно защититься.
+          </p>
+        </li>
+      </ul>
+      <div className="about-project__progress">
+        <div className="progress__section progress__section_active">
+          <span className="progress__text progress__text_active">1 неделя</span>
+        </div>
+        <div className="progress__section">
+          <span className="progress__text">4 недели</span>
+        </div>
+      </div>
+      <ul className="about-project__knowleges">
+        <li className="about-project__knowledge">Back-end</li>
+        <li className="about-project__knowledge">Front-end</li>
+      </ul>
+    </section>
+  );
 }
 
 export default AboutProject;
