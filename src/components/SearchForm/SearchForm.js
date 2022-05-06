4c6d60591d45ab1f7bf83f0223bf89e0de575401
Tanya <pinki_00@mail.ru>
2022-05-06 22:52:58 +0300

    Add search form

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,7 +1,36 @@
+import searchImg from "../../images/search.svg";
 import "./SearchForm.css";
 
 function SearchForm() {
-  return <div>SearchForm</div>;
+  return (
+    <section className="search">
+      <form className="search__form">
+        <div className="search__container">
+          <input
+            className="search__input"
+            type="text"
+            name="movie"
+            id="movie"
+            placeholder="Фильм"
+          ></input>
+          <button type="submit" className="search__submit">
+            <img className="search__img" src={searchImg} alt="search"></img>
+          </button>
+        </div>
+        <label className="search__checkbox">
+          Короткометражки
+          <input
+            className="checkbox__input"
+            type="checkbox"
+            р
+            name="short-movie"
+            id="short-movie"
+          ></input>
+          <div className="checkbox__wrapper"></div>
+        </label>
+      </form>
+    </section>
+  );
 }
 
 export default SearchForm;
