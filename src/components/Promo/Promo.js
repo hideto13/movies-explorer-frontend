19917e9f749131fc7bc28ac8dc5aaa4b71d6ea7f
Tanya <pinki_00@mail.ru>
2022-05-12 22:49:19 +0300

    Fix adaptive design

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -4,11 +4,12 @@ import "./Promo.css";
 function Promo() {
   return (
     <section className="promo">
-      <h1 className="promo__title">
-        Учебный проект студента факультета Веб-разработки.
-      </h1>
-
-      <NavTab />
+      <div className="promo__container">
+        <h1 className="promo__title">
+          Учебный проект студента факультета Веб-разработки.
+        </h1>
+        <NavTab />
+      </div>
     </section>
   );
 }
