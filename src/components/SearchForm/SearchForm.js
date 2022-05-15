d915ccfdd7e0c9a556874084de14efedf8b46fe7
Tanya <pinki_00@mail.ru>
2022-05-15 22:06:34 +0300

    Get movies api

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,10 +1,31 @@
+import React from "react";
 import searchImg from "../../images/search.svg";
 import "./SearchForm.css";
 
-function SearchForm() {
+function SearchForm({ onSearchMovie }) {
+  const [searchValue, setSearchValue] = React.useState("");
+  const [errorText, setErrorText] = React.useState("");
+
+  function handleSearchValueChange(e) {
+    setSearchValue(e.target.value);
+    setErrorText("");
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (searchValue === "") {
+      setErrorText("Нужно ввести ключевое слово");
+    } else {
+      onSearchMovie({
+        searchValue,
+      });
+    }
+  }
+
   return (
     <section className="search">
-      <form className="search__form">
+      <form className="search__form" onSubmit={handleSubmit} noValidate>
         <div className="search__container">
           <input
             className="search__input"
@@ -13,10 +34,15 @@ function SearchForm() {
             id="movie"
             required
             placeholder="Фильм"
+            onChange={handleSearchValueChange}
+            value={searchValue || ""}
           ></input>
           <button type="submit" className="search__submit">
             <img className="search__img" src={searchImg} alt="search"></img>
           </button>
+          <span id="text-error" className="search__error">
+            {errorText}
+          </span>
         </div>
         <label className="search__checkbox">
           Короткометражки
