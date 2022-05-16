20a6c737d0ce151802c632d1436d1618d15337a7
Tanya <pinki_00@mail.ru>
2022-05-16 23:12:04 +0300

    View movies card from api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://api.tkozlova-diplom.nomoredomains.xyz";
-export const SEARCH_MOVIES_URL = "https://api.nomoreparties.co/beatfilm-movies";
+export const MAIN_URL = "https://api.tkozlova-diplom.nomoredomains.xyz";
+export const MOVIES_URL = "https://api.nomoreparties.co/beatfilm-movies";
